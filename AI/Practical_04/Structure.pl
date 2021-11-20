afc7de25b2c5f3959ed2321c230003f82b9914c9
Yash Gupta <57324872+YG3A5D@users.noreply.github.com>
2021-11-20 12:14:50 +0530

    Structure.pl

---
+++ AI/Practical_04/Structure.pl
@@ -0,0 +1,8 @@
+owns(john,book(ai,author(Galvin, Heigh))).
+owns(marry,book(ml,author(Tewani,Born))).
+
+owns(john,book(X,author(Galvin,Heigh))).
+owns(marry,book(Y,author(Tewani,Born))).
+
+owns(john,book(_,author(Galvin,Heigh))).
+owns(marry,book(_,author(Tewani,Born))).
