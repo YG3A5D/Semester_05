f221ffb2a91564dda5c0218a5f6e59fc269172c9
Yash Gupta <57324872+YG3A5D@users.noreply.github.com>
2021-11-20 12:49:47 +0530

    Create Operator_Precedence.pl

---
+++ AI/Practical_04/Operator_Precedence.pl
@@ -0,0 +1,9 @@
+%Practical_Lab_for_Operator
+here(now)=now.
+%Example_of_Operator_Precedence
+'student'=student.
+"student"="student".
+
+%Example_for_
+f(X,X)=f(a,b). % => FALSE
+f(X,Y)=f(a,b). % => X = a ; Y = b ;
