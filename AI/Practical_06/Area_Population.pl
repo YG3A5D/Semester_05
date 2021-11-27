19960204875e9d373d3236b65387ee19901d612c
Yash Gupta <57324872+YG3A5D@users.noreply.github.com>
2021-11-27 12:34:55 +0530

    Create Area_Population.pl

---
+++ AI/Practical_06/Area_Population.pl
@@ -0,0 +1,19 @@
+% Author: 2019BTCS088
+% Lab: SATURDAY ; 27-11-2021
+% Example_Density_Calculation_02
+
+% Declaring Facts
+population('China',100).
+population('India',85).
+population('USA',40).
+population('Pakistan',9).
+
+% Declaring Facts
+area('China',10).
+area('India',4).
+area('USA',12).
+area('Pakistan',2).
+
+density(X,Y):-population(X,Pop),
+              area(X,Ar),
+	            Y is Pop/Ar.
