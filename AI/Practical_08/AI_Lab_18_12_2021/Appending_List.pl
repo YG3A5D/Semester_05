fef36e518fef0491c4ca6206b1c43417745d1144
Yash Gupta <57324872+YG3A5D@users.noreply.github.com>
2022-01-05 12:29:03 +0530

    Updated Concatenating_list.pl

--- AI/Practical_08/AI_Lab_18_12_2021/Appending_List.pl
+++ AI/Practical_08/AI_Lab_18_12_2021/Appending_List.pl
@@ -2,6 +2,9 @@
 %Author: 2019BTCS088
 %Topic: CONCATENATING_TWO_LISTS
 
-append([],L2,L2).
-append([H|T],L2,[H|L3]):-
-	append(T,L2,L3).
+% When first list => EMPTY & second list => L then final list after concatenation => 'L' list only.
+concatenate_list([],L2,L2).
+
+% When the first list != empty, then write this as [Head|Tail], concatenate Tail with L2 recursively, and store into new list in the form, [Head|New List].
+concatenate_list([H|T],L2,[H|L3]):-
+	concatenate_list(T,L2,L3).
